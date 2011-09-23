217f431854b75c833bd55ba890ba773b5572f1fc
Arik Devens <arik@posterous-inc.com>
2011-09-23 12:48:14 -0700

    [FIX #13] Added accessor methods for all the cases we support.

--- Pilot/Pilot.h
+++ Pilot/Pilot.h
@@ -18,7 +18,47 @@
 /**
  *
  */
-+ (void)showObject:(PTObject *)model withSelector:(SEL)selector animation:(UIViewAnimationTransition)transition asModal:(BOOL)asModal;
++ (void)showObject:(PTObject *)object;
+
+/**
+ *
+ */
++ (void)showObjectAsModal:(PTObject *)object;
+
+/**
+ *
+ */
++ (void)showObject:(PTObject *)object withSelector:(SEL)selector;
+
+/**
+ *
+ */
++ (void)showObjectAsModal:(PTObject *)object withSelector:(SEL)selector;
+
+/**
+ *
+ */
++ (void)showObject:(PTObject *)object animation:(UIViewAnimationTransition)animation;
+
+/**
+ *
+ */
++ (void)showObjectAsModal:(PTObject *)object animation:(UIViewAnimationTransition)animation;
+
+/**
+ *
+ */
++ (void)showObject:(PTObject *)object withSelector:(SEL)selector animation:(UIViewAnimationTransition)animation;
+
+/**
+ *
+ */
++ (void)showObjectAsModal:(PTObject *)object withSelector:(SEL)selector animation:(UIViewAnimationTransition)animation;
+
+/**
+ *
+ */
++ (void)showObject:(PTObject *)object withSelector:(SEL)selector animation:(UIViewAnimationTransition)animation asModal:(BOOL)asModal;
 
 /**
  * Returns the [[UIApplication sharedApplication] delegate]
--- Pilot/Pilot.m
+++ Pilot/Pilot.m
@@ -10,46 +10,83 @@
 
 
 @interface Pilot (hidden)
-+ (Class)viewControllerClassForModel:(id)model;
-+ (void)presentViewController:(id)viewController;
-+ (void)presentViewControllerAsModal:(id)viewController;
++ (Class)viewControllerClassForObject:(PTObject *)object;
++ (SEL)defaultInitializer;
++ (void)presentViewController:(id)viewController withAnimation:(UIViewAnimationTransition)animation;
++ (void)presentViewControllerAsModal:(id)viewController withAnimation:(UIViewAnimationTransition)animation;
 @end
 
 @implementation Pilot
 
 #pragma mark - Private Methods
 
-+ (Class)viewControllerClassForModel:(id)model {
-    NSString *modelClassName = NSStringFromClass([model class]);
-    NSString *viewControllerClassName = [NSString stringWithFormat:@"%@ViewController", modelClassName];
++ (Class)viewControllerClassForObject:(PTObject *)object {
+    NSString *objectClassName = NSStringFromClass([object class]);
+    NSString *viewControllerClassName = [NSString stringWithFormat:@"%@ViewController", objectClassName];
     
-    NSAssert(NSClassFromString(viewControllerClassName), @"PILOT ERROR: Could not find %@ViewController", modelClassName);
+    NSAssert(NSClassFromString(viewControllerClassName), @"PILOT ERROR: Could not find %@ViewController", objectClassName);
     
     return NSClassFromString(viewControllerClassName);
 }
 
-+ (void)presentViewController:(id)viewController {
++ (SEL)defaultInitializer {
+    return NSSelectorFromString(@"initWithObjectIdentifier:");
+}
+
++ (void)presentViewController:(id)viewController withAnimation:(UIViewAnimationTransition)animation {
     [[self navigationController] pushViewController:viewController animated:YES];
 }
 
-+ (void)presentViewControllerAsModal:(id)viewController {
++ (void)presentViewControllerAsModal:(id)viewController withAnimation:(UIViewAnimationTransition)animation {
     [[self navigationController] presentModalViewController:viewController animated:YES];
 }
 
 #pragma mark - Public API
 
-+ (void)showObject:(PTObject *)model withSelector:(SEL)selector animation:(UIViewAnimationTransition)transition asModal:(BOOL)asModal {
-    Class viewControllerClass = [self viewControllerClassForModel:model];
++ (void)showObject:(PTObject *)object {
+    [self showObject:object withSelector:[self defaultInitializer] animation:UIViewAnimationTransitionNone asModal:NO];
+}
+
++ (void)showObjectAsModal:(PTObject *)object {
+    [self showObject:object withSelector:[self defaultInitializer] animation:UIViewAnimationTransitionNone asModal:YES];
+}
+
++ (void)showObject:(PTObject *)object withSelector:(SEL)selector {
+    [self showObject:object withSelector:selector animation:UIViewAnimationTransitionNone asModal:NO];
+}
+
++ (void)showObjectAsModal:(PTObject *)object withSelector:(SEL)selector {
+    [self showObject:object withSelector:selector animation:UIViewAnimationTransitionNone asModal:YES];
+}
+
++ (void)showObject:(PTObject *)object animation:(UIViewAnimationTransition)animation {
+    [self showObject:object withSelector:[self defaultInitializer] animation:animation asModal:NO];
+}
+
++ (void)showObjectAsModal:(PTObject *)object animation:(UIViewAnimationTransition)animation {
+    [self showObject:object withSelector:[self defaultInitializer] animation:animation asModal:YES];
+}
+
++ (void)showObject:(PTObject *)object withSelector:(SEL)selector animation:(UIViewAnimationTransition)animation {
+    [self showObject:object withSelector:selector animation:animation asModal:NO];
+}
+
++ (void)showObjectAsModal:(PTObject *)object withSelector:(SEL)selector animation:(UIViewAnimationTransition)animation {
+    [self showObject:object withSelector:selector animation:animation asModal:YES];
+}
+
++ (void)showObject:(PTObject *)object withSelector:(SEL)selector animation:(UIViewAnimationTransition)animation asModal:(BOOL)asModal {
+    Class viewControllerClass = [self viewControllerClassForObject:object];
 
     NSAssert([viewControllerClass instancesRespondToSelector:selector], @"PILOT ERROR: Could not find selector: %@ for %@ViewController", 
-             NSStringFromSelector(selector), NSStringFromClass([model class])]);
+             NSStringFromSelector(selector), NSStringFromClass([object class])]);
     
-    id viewController = [[[viewControllerClass alloc] performSelector:selector withObject:model.identifier] autorelease];
+    id viewController = [[[viewControllerClass alloc] performSelector:selector withObject:object.identifier] autorelease];
     
     if (asModal) {
-        [self presentViewControllerAsModal:viewController];
+        [self presentViewControllerAsModal:viewController withAnimation:animation];
     } else {
-        [self presentViewController:viewController];
+        [self presentViewController:viewController withAnimation:animation];
     }
 }
 
